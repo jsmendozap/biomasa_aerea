2e4525a3c9297f11b509d30971fbaec8bf7b80ab
jsmendozap <jsmendozap@unal.edu.co>
2022-04-06 20:56:56 -0500

    Procesado de imagen SAR

---
+++ Procesado imágenes SAR.R
@@ -0,0 +1,74 @@
+setwd("D:/Drive/Programación/R/R SIG/Biomasa Aérea con imágenes SAR y R/Resultados")
+
+# Se tienen imágenes SAR descargadas desde GEE las cuales ya se encuentran corregidas
+# geométrica y radiométricamente. Con ellas se realiza el siguiente el procesamiento:
+# 1. Convertir los valores digitales a decibeles.
+# 2. Realizar un filtro para disminuir el ruido.
+# 3. Proyectar el sistema de coordenadas a planas.
+
+# Cargando librerias
+
+library(sf)
+library(raster)
+
+# Cargando raster de las imágenes SAR
+
+raster <- stack(c("Mosaico.tif", "bosque_noBosque.tif"))
+names(raster)[1:2] <- c("HH", "HV")
+
+# Guardando archivos con valores de retrodispersión de las polarizaciones
+
+dir.create("retrodispersión")
+
+ret <- function(x) { # Convierte los valores digitales a coeficientes de retrodispersión
+  10 * log10(x^2) - 83
+}
+
+for (i in 1:2) {
+  calc(
+    x = raster[[i]], fun = ret,
+    filename = paste("retrodispersión/", names(raster)[i], sep = ""),
+    format = "GTiff"
+  )
+}
+
+HH <- raster("retrodispersión/HH.tif")
+HV <- raster("retrodispersión/HH.tif")
+plot(HH, col = gray.colors(30, start = 0.3, end = 1), legend = T)
+plot(HV, col = gray.colors(30, start = 0.3, end = 1), legend = T)
+
+# Aplicación del filtro de Lee para reducción de ruido
+
+dir.create("Lee")
+
+library(whitebox)
+install_whitebox() # La primera vez que se instala la libreria se debe correr el comando para descargar el ejecutable
+
+for (i in 1:2) {
+  wbt_lee_sigma_filter(
+    input = paste(getwd(), "/", dir("retrodispersión", full.names = T)[i], sep = ""),
+    output = paste(getwd(), "/Lee/", dir("retrodispersión/")[i], sep = ""),
+    filterx = 3, filtery = 3
+  )
+}
+
+# Proyectando las imágenes a Magna Colombia-Bogotá EPSG: 3116 - Coordenadas planas
+
+imagenes <- list()
+
+for (i in 1:2) {
+  img <- raster(dir("Lee/", full.names = T)[i])
+  imagenes[[i]] <- projectRaster(
+    from = img, crs = "EPSG:3116",
+    filename = dir("Lee/")[i], overwrite = T
+  )
+}
+
+multibanda <- stack(imagenes)
+writeRaster(x = multibanda, filename = "Procesada", format = "GTiff")
+plotRGB(multibanda, r = 1, g = 2, b = 1, axes = T, stretch = "lin")
+
+b_nb <- projectRaster(
+  from = raster[[3]], crs = "EPSG:3116", filename = "b_nb",
+  overwrite = T, format = "GTiff"
+)
